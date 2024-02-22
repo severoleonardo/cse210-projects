48f3210fdde19996cea1483d1104410d587669a8
severoleonardo <2020.leonardosevero@gmail.com>
2024-02-22 20:58:14 -0300

    Refactor Category class constructor to include a comment

--- final/FinalProject/Category.cs
+++ final/FinalProject/Category.cs
@@ -1,8 +1,8 @@
-public class Category
+public class Category // Represents a single category for a transaction
 {
     public string Name { get; private set; }
 
-    public Category(string name)
+    public Category(string name) // Initializes a new instance of the Category class.
     {
         Name = name;
     }
